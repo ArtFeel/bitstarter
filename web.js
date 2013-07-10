cc30f88a7305785677b16f0fdc1728bfbb65df5a
Philip Vasilchenko <philip.vasilchenko@gmail.com>
2013-07-10 21:08:14 +0300

    Read response from file

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	var responseString = fs.readFileSync("index.html").toString();
+  	response.send(responseString);
 });
 
 var port = process.env.PORT || 5000;
